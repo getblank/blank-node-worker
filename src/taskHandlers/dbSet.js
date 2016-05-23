41931bfeaf2e168497b091705696192e9b312654
kib357 <kib357@gmail.com>
2016-05-23 16:36:40 +0500

    dbGet&dbSet handlers for display:single

--- src/taskHandlers/dbSet.js
+++ src/taskHandlers/dbSet.js
@@ -1,13 +1,31 @@
 "use strict";
 
 import TaskHandlerBase from "./TaskHandlerBase";
+import configStore from "../configStore";
 
 class DbSet extends TaskHandlerBase {
+    __checkItemId(storeName, user, item, cb) {
+        let storeDesc = configStore.getStoreDesc(storeName, user);
+        if (storeDesc.display === "single") {
+            this.db.get({ "_ownerId": user._id }, storeName, (e, d) => {
+                if (e) {
+                    cb(this.db.newId());
+                } else {
+                    cb(d._id);
+                }
+            });
+        } else {
+            cb(item._id);
+        }
+    }
     run(storeName, user, args, cb) {
         if (args == null || !args.item || !args.item._id) {
             throw new Error("Invalid args.");
         }
-        this.db.set(args.item, storeName, {"user": user}, cb);
+        this.__checkItemId(storeName, user, args.item, (id) => {
+            args.item._id = id;
+            this.db.set(args.item, storeName, { "user": user }, cb);
+        });
     }
 }
 let dbSet = new DbSet();
