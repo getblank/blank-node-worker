1653bca4dd41aab9e80131f423d577427ced2aef
Evgeniy Ivakha <ivahaev@gmail.com>
2018-10-16 18:39:24 +0500

    sync updated

--- tests/syncTests.js
+++ tests/syncTests.js
@@ -3,9 +3,9 @@
 const assert = require("assert");
 const sync = require("../lib/sync");
 
-describe("SyncTest", function () {
-    describe("#lock", function () {
-        it("should call cb after module setup", function (done) {
+describe("SyncTest", function() {
+    describe("#lock", function() {
+        it("should call cb after module setup", function(done) {
             let _async = false;
             sync.lock("1", () => {
                 assert.equal(_async, true);
@@ -17,8 +17,8 @@ describe("SyncTest", function () {
             _async = true;
         });
 
-        it("should return unlock function", function (done) {
-            sync.lock("1", (unlock) => {
+        it("should return unlock function", function(done) {
+            sync.lock("1", unlock => {
                 unlock();
             });
             sync.setup({
@@ -35,15 +35,35 @@ describe("SyncTest", function () {
             });
         });
 
-        it("should throws an error when unlock called more then one time", function (done) {
+        it("should throws an error when unlock called more then one time", function(done) {
             sync.setup({
                 call: (m, cb, id) => cb(),
             });
-            sync.lock("2", (unlock) => {
+            sync.lock("2", unlock => {
                 unlock();
                 assert.throws(unlock, /Attempt to unlock no locked mutex/);
                 done();
             });
+        });
+    });
+
+    describe("#once", () => {
+        it("should call without error only once", () => {
+            const id = "1234567890";
+            let calls = 0;
+            return sync
+                .once(id)
+                .then(() => {
+                    calls++;
+                    return sync.once(id);
+                })
+                .then(() => {
+                    calls++;
+                })
+                .catch(err => {})
+                .then(() => {
+                    assert.equal(calls, 1, "should be one");
+                });
         });
     });
 });
